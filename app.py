e0656cf0606701114f41d2bb9897c83f14554586
akhilkumarguntupalli1 <akhil200316@gmail.com>
2025-07-28 15:48:28 +0530

    Fix: Bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -661,5 +661,7 @@ if __name__ == '__main__':
     scheduler_thread.start()
     
     import os
-    port = int(os.environ.get('PORT', 5000))
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Use port from Render
     app.run(host='0.0.0.0', port=port, debug=True)
