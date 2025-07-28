586505e62f94ecbb0a52ce3f38842589a8efef98
akhilkumarguntupalli1 <akhil200316@gmail.com>
2025-07-28 15:40:32 +0530

    Update port binding for Render

--- app.py
+++ app.py
@@ -660,4 +660,6 @@ if __name__ == '__main__':
     scheduler_thread.daemon = True
     scheduler_thread.start()
     
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
